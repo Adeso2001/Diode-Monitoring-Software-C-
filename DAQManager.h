add07eeffac5fdd38cc7318dc4738631510715b8
Adeso2001 <oliverjamieades@gmail.com>
2026-02-25 21:50:59 +0000

    Added function declarations for DAQ Manager Class

--- DAQManager.h
+++ DAQManager.h
@@ -24,20 +24,20 @@ class DAQManager
     private:
 
     // Hat information
-    const static int num_hats{2};
-    const static vector<uint8_t> hat_address_vector {0, 1};
-    const static bool single_ended_mode{true};
-    const static int voltage_range_pm{10}; // volts
-    vector<bool> is_reading;
+    const static int num_hats{2}; // number of DAQ HATs connected (assumed to be 2)
+    const static vector<uint8_t> hat_address_vector {0, 1}; // address of each DAQ HAT
+    const static bool single_ended_mode{true}; // true if the DAQ HAT is in single ended mode, false if it is in differential mode
+    const static int voltage_range_pm{10}; // voltage range in volts (assumed to be +/- 10V)
+    bool is_reading; // true if the DAQ HAT is reading data, false if it is not
 
     // Channel information
-    vector<std::unique_ptr<Calibration>> calibration_vector;
-    vector<bool> is_calibrated;
+    vector<std::unique_ptr<Calibration>> calibration_vector; // vector of calibration objects for each channel
+    vector<bool> is_calibrated; // true if the channel is calibrated, false if it is not
     
     // Temp data storage
-    double time_begin;
-    long averages_performed;
-    vector<vector<double>> temp_data_vector; // stores data less than 1000
+    double time_begin; // time the reading began
+    long averages_performed; // number of averages performed, used to calculate time values
+    vector<vector<double>> temp_data_vector; // stores data if there arent enough reading for averaging
 
     // sampling constants
     const static double sample_rate = 12500; // samples per second
@@ -46,12 +46,34 @@ class DAQManager
 
     public:
 
+    // constructor
+    DAQManager();
 
+    // destructor
+    ~DAQManager();
 
+    // getters
+    int get_num_hats() const;
+    vector<uint8_t> get_hat_address_vector() const;
+    bool get_single_ended_mode() const;
 
+    int get_voltage_range_pm() const;
+    double get_sample_rate() const;
+    double get_averaging_time() const;
+    int get_averaging_samples() const;
+    long get_averages_performed() const;
+    vector<vector<double>> get_temp_data_vector() const;
 
+    // calibration functions
+    void calibrate_from_vector(int channel_number, vector<double> temperature_vector, vector<double> voltage_vector);
+    void calibrate_from_file(int channel_number, string file_name);
+    bool is_calibrated(int channel_number) const;
 
+    // operational functions
+    void start_reading(); // start reading data from the DAQ HATs
+    void stop_reading(); // stop reading data from the DAQ HATs and perform cleanup
 
+    vector<vector<double>> read_data() const; // return averaged data from set number of DAQ HAT readings
 
 };
 #endif
