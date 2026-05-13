02bcacc3958d0ebcc472ecbe12d89a76b8f2bd4b
Adeso2001 <oliverjamieades@gmail.com>
2026-05-13 11:30:43 +0100

    Fixed change not working on linux

--- applicationTest.cpp
+++ applicationTest.cpp
@@ -1,5 +1,33 @@
 #include "DiodeProgram.h"
-#include <conio.h>
+#include <iostream>
+
+#ifdef _WIN32
+    #include <conio.h>
+#else
+    #include <termios.h>
+    #include <unistd.h>
+#endif
+
+int getch_nonblocking() {
+    #ifdef _WIN32
+        if (_kbhit()) {
+            return _getch();
+        }
+        return -1;
+    #else
+        struct termios oldt, newt;
+        int ch;
+        tcgetattr(STDIN_FILENO, &oldt);
+        newt = oldt;
+        newt.c_lflag &= ~(ICANON | ECHO);
+        newt.c_cc[VMIN] = 0;
+        newt.c_cc[VTIME] = 0;
+        tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+        ch = getchar();
+        tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+        return (ch == EOF) ? -1 : ch;
+    #endif
+}
 
 int main()
 {
@@ -8,9 +36,9 @@ int main()
     char input = ' ';
     while(input != 'q' && input != 'Q')
     {
-        if(_kbhit())
-        {
-            input = _getch();
+        int key = getch_nonblocking();
+        if (key != -1) {
+            input = (char)key;
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
